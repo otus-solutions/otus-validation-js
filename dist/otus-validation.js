7cf32b58c4b028c56d12c0edfd563f4763be70d1
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2017-02-06 19:48:18 -0200

    Atualização dist otus-validation

--- dist/otus-validation.js
+++ dist/otus-validation.js
@@ -701,6 +701,98 @@
 }());
 
 (function() {
+  'use strict';
+
+  angular
+    .module('otus.validation')
+    .service('MaxTimeValidatorService', MaxTimeValidatorService);
+
+  MaxTimeValidatorService.$inject = ['ValidatorResponseFactory'];
+
+  function MaxTimeValidatorService(ValidatorResponseFactory) {
+    var self = this;
+    self.execute = execute;
+
+    function execute(answer, data) {
+      if (angular.equals(answer.data, {})) {
+        return ValidatorResponseFactory.create(answer, data, true);
+      }
+      var result = _compareTime(answer.data, data.reference);
+      return ValidatorResponseFactory.create(answer, data, result);
+    }
+
+    /**
+    *
+    * TODO When the param reference be standardized on Studio(Time validator)
+    * the setters below won't be necessary.
+    if
+      reference is miliseconds - remove dateReference and getTime
+    else
+      remove dateReference and keep dateTime
+    */
+
+    function _compareTime(answer, reference) {
+      var dateReference = new Date(reference);
+      dateReference.setDate(1);
+      dateReference.setMonth(0);
+      dateReference.setFullYear(1970);
+
+      var ansDate = new Date(answer);
+      ansDate.setDate(1);
+      ansDate.setMonth(0);
+      ansDate.setFullYear(1970);
+
+      return ansDate.getTime() <= dateReference.getTime();
+    }
+
+  }
+
+}());
+
+(function() {
+    'use strict';
+
+    angular
+        .module('otus.validation')
+        .service('MinTimeValidatorService', MinTimeValidatorService);
+
+    MinTimeValidatorService.$inject = ['ValidatorResponseFactory'];
+
+    function MinTimeValidatorService(ValidatorResponseFactory) {
+        var self = this;
+        self.execute = execute;
+
+        function execute(answer, data) {
+            if (angular.equals(answer.data, {})) {
+                return ValidatorResponseFactory.create(answer, data, true);
+            }
+            var result = _compareTime(answer.data, data.reference);
+            return ValidatorResponseFactory.create(answer, data, result);
+        }
+
+        /**
+        *
+        * TODO When the param reference be standardized on Studio(Time validator)
+        * the setters below won't be necessary.
+        if
+          reference is miliseconds - remove dateReference and getTime
+        else
+          remove dateReference and keep dateTime
+        */
+        function _compareTime(answer, reference) {
+            var dateReference = new Date(reference);
+
+            dateReference.setDate(1);
+            dateReference.setMonth(0);
+            dateReference.setFullYear(1970);
+
+            return answer >= dateReference.getTime();
+        }
+    }
+
+}());
+
+(function() {
     'use strict';
 
     angular
@@ -832,98 +924,6 @@
 
         function execute(answer, data) {
           return ValidatorResponseFactory.create(answer, data, true);
-        }
-    }
-
-}());
-
-(function() {
-  'use strict';
-
-  angular
-    .module('otus.validation')
-    .service('MaxTimeValidatorService', MaxTimeValidatorService);
-
-  MaxTimeValidatorService.$inject = ['ValidatorResponseFactory'];
-
-  function MaxTimeValidatorService(ValidatorResponseFactory) {
-    var self = this;
-    self.execute = execute;
-
-    function execute(answer, data) {
-      if (angular.equals(answer.data, {})) {
-        return ValidatorResponseFactory.create(answer, data, true);
-      }
-      var result = _compareTime(answer.data, data.reference);
-      return ValidatorResponseFactory.create(answer, data, result);
-    }
-
-    /**
-    *
-    * TODO When the param reference be standardized on Studio(Time validator)
-    * the setters below won't be necessary.
-    if
-      reference is miliseconds - remove dateReference and getTime
-    else
-      remove dateReference and keep dateTime
-    */
-
-    function _compareTime(answer, reference) {
-      var dateReference = new Date(reference);
-      dateReference.setDate(1);
-      dateReference.setMonth(0);
-      dateReference.setFullYear(1970);
-
-      var ansDate = new Date(answer);
-      ansDate.setDate(1);
-      ansDate.setMonth(0);
-      ansDate.setFullYear(1970);
-
-      return ansDate.getTime() <= dateReference.getTime();
-    }
-
-  }
-
-}());
-
-(function() {
-    'use strict';
-
-    angular
-        .module('otus.validation')
-        .service('MinTimeValidatorService', MinTimeValidatorService);
-
-    MinTimeValidatorService.$inject = ['ValidatorResponseFactory'];
-
-    function MinTimeValidatorService(ValidatorResponseFactory) {
-        var self = this;
-        self.execute = execute;
-
-        function execute(answer, data) {
-            if (angular.equals(answer.data, {})) {
-                return ValidatorResponseFactory.create(answer, data, true);
-            }
-            var result = _compareTime(answer.data, data.reference);
-            return ValidatorResponseFactory.create(answer, data, result);
-        }
-
-        /**
-        *
-        * TODO When the param reference be standardized on Studio(Time validator)
-        * the setters below won't be necessary.
-        if
-          reference is miliseconds - remove dateReference and getTime
-        else
-          remove dateReference and keep dateTime
-        */
-        function _compareTime(answer, reference) {
-            var dateReference = new Date(reference);
-
-            dateReference.setDate(1);
-            dateReference.setMonth(0);
-            dateReference.setFullYear(1970);
-
-            return answer >= dateReference.getTime();
         }
     }
 
