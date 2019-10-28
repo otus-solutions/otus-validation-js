b66f8647824a6243a8d3d8f3e7f4d92946346004
Diogo Ferreira <diogo@otus-solutions.com.br>
2019-10-28 12:56:17 -0300

    Script Production

--- dist/otus-validation.js
+++ dist/otus-validation.js
@@ -791,6 +791,72 @@
 }());
 
 (function() {
+  'use strict';
+
+  angular
+    .module('otus.validation')
+    .service('MaxTimeValidatorService', MaxTimeValidatorService);
+
+  MaxTimeValidatorService.$inject = [
+     'ValidatorResponseFactory',
+     'GlobalTimeService'
+  ];
+
+  function MaxTimeValidatorService(ValidatorResponseFactory, GlobalTimeService) {
+    var self = this;
+    self.execute = execute;
+
+    function execute(answer, data) {
+      if (angular.equals(answer.data, {})) {
+        return ValidatorResponseFactory.create(answer, data, true);
+      }
+      var result = _compareTime(answer.data, data.reference.value);
+      return ValidatorResponseFactory.create(answer, data, result);
+    }
+
+    function _compareTime(answer, reference) {
+      var dateReference = GlobalTimeService.getMiliTime(reference);
+      var ansRef = GlobalTimeService.getMiliTime(answer);
+      return ansRef <= dateReference;
+    }
+  }
+
+}());
+
+(function() {
+  'use strict';
+
+  angular
+    .module('otus.validation')
+    .service('MinTimeValidatorService', MinTimeValidatorService);
+
+  MinTimeValidatorService.$inject = [
+     'ValidatorResponseFactory',
+     'GlobalTimeService'
+  ];
+
+  function MinTimeValidatorService(ValidatorResponseFactory, GlobalTimeService) {
+    var self = this;
+    self.execute = execute;
+
+    function execute(answer, data) {
+      if (angular.equals(answer.data, {})) {
+        return ValidatorResponseFactory.create(answer, data, true);
+      }
+      var result = _compareTime(answer.data, data.reference.value);
+      return ValidatorResponseFactory.create(answer, data, result);
+    }
+
+    function _compareTime(answer, reference) {
+      var dateReference = GlobalTimeService.getMiliTime(reference);
+      var ansRef = GlobalTimeService.getMiliTime(answer);
+      return ansRef >= dateReference;
+    }
+  }
+
+}());
+
+(function() {
     'use strict';
 
     angular
@@ -924,72 +990,6 @@
           return ValidatorResponseFactory.create(answer, data, true);
         }
     }
-
-}());
-
-(function() {
-  'use strict';
-
-  angular
-    .module('otus.validation')
-    .service('MaxTimeValidatorService', MaxTimeValidatorService);
-
-  MaxTimeValidatorService.$inject = [
-     'ValidatorResponseFactory',
-     'GlobalTimeService'
-  ];
-
-  function MaxTimeValidatorService(ValidatorResponseFactory, GlobalTimeService) {
-    var self = this;
-    self.execute = execute;
-
-    function execute(answer, data) {
-      if (angular.equals(answer.data, {})) {
-        return ValidatorResponseFactory.create(answer, data, true);
-      }
-      var result = _compareTime(answer.data, data.reference.value);
-      return ValidatorResponseFactory.create(answer, data, result);
-    }
-
-    function _compareTime(answer, reference) {
-      var dateReference = GlobalTimeService.getMiliTime(reference);
-      var ansRef = GlobalTimeService.getMiliTime(answer);
-      return ansRef <= dateReference;
-    }
-  }
-
-}());
-
-(function() {
-  'use strict';
-
-  angular
-    .module('otus.validation')
-    .service('MinTimeValidatorService', MinTimeValidatorService);
-
-  MinTimeValidatorService.$inject = [
-     'ValidatorResponseFactory',
-     'GlobalTimeService'
-  ];
-
-  function MinTimeValidatorService(ValidatorResponseFactory, GlobalTimeService) {
-    var self = this;
-    self.execute = execute;
-
-    function execute(answer, data) {
-      if (angular.equals(answer.data, {})) {
-        return ValidatorResponseFactory.create(answer, data, true);
-      }
-      var result = _compareTime(answer.data, data.reference.value);
-      return ValidatorResponseFactory.create(answer, data, result);
-    }
-
-    function _compareTime(answer, reference) {
-      var dateReference = GlobalTimeService.getMiliTime(reference);
-      var ansRef = GlobalTimeService.getMiliTime(answer);
-      return ansRef >= dateReference;
-    }
-  }
 
 }());
 
